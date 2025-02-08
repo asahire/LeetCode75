2d29e30831f1c629780de0600d1678a8ba54856e
Atul Ahire <atulsureshahire@gmail.com>
2025-02-08 03:18:33 -0800

    Time: 5 ms (58.24%) | Memory: 44.5 MB (91.84%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -9,31 +9,50 @@ class Solution {
         map.put('D', 500);
         map.put('M', 1000);
 
-        int length =s.length();
-        int numericValue=0;
+        // int length =s.length();
+        // int numericValue=0;
 
-        while(length > 0)
-        {   
-           // if(length == 1) return numericValue + map.get(s.charAt(length-1));
+        // while(length > 0)
+        // {   
+        //    // if(length == 1) return numericValue + map.get(s.charAt(length-1));
 
-            char romanChar = s.charAt(length-1);
-            // System.out.println(romanChar);
-            numericValue += map.get(romanChar);
+        //     char romanChar = s.charAt(length-1);
+        //     // System.out.println(romanChar);
+        //     numericValue += map.get(romanChar);
 
-            // handle special cases of IV, XC etc.
-            if(length >= 2)
-            {
-                if(map.get(romanChar) > map.get(s.charAt(length-2)))
-                {
-                    numericValue -= map.get(s.charAt(length-2));
-                    length--;
-                }
+        //     // handle special cases of IV, XC etc.
+        //     if(length >= 2)
+        //     {
+        //         if(map.get(romanChar) > map.get(s.charAt(length-2)))
+        //         {
+        //             numericValue -= map.get(s.charAt(length-2));
+        //             length--;
+        //         }
+        //     }
+
+        //     length--;    
+        // }
+
+        // return numericValue;
+
+                int total = 0;
+        int prevValue = 0; // Keep track of the last Roman numeral value
+
+        // Step 2: Iterate through the string
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = map.get(s.charAt(i));
+
+            // Step 3: Check if we need to subtract or add the value
+            if (currValue < prevValue) {
+                total -= currValue; // Subtract
+            } else {
+                total += currValue; // Add
             }
 
-            length--;    
+            prevValue = currValue; // Update previous value
         }
 
-        return numericValue;
+        return total;
 
     }
 }
