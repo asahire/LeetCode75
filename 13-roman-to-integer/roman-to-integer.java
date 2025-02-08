4ffca26836b879d559113bc5161d1c1598384557
Atul Ahire <atulsureshahire@gmail.com>
2025-02-08 03:24:21 -0800

    Time: 3 ms (83.64%) | Memory: 44.6 MB (83.07%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,13 +1,26 @@
 class Solution {
+     private int getValue(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // map.put('I', 1);
+        // map.put('V', 5);
+        // map.put('X', 10);
+        // map.put('L', 50);
+        // map.put('C', 100);
+        // map.put('D', 500);
+        // map.put('M', 1000);
 
         // int length =s.length();
         // int numericValue=0;
@@ -35,24 +48,39 @@ class Solution {
 
         // return numericValue;
 
-                int total = 0;
-        int prevValue = 0; // Keep track of the last Roman numeral value
+    //    int sum = 0;
+    //     int n = s.length();
+        
+    //     for (int i = 0; i < n; i++) {
+    //         int current = getValue(s.charAt(i));
+            
+    //         // Check if the next symbol represents a larger number
+    //         if (i + 1 < n && current < getValue(s.charAt(i + 1))) {
+    //             sum -= current;
+    //         } else {
+    //             sum += current;
+    //         }
+    //     }
+    //     return sum;
 
-        // Step 2: Iterate through the string
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int currValue = map.get(s.charAt(i));
+    
 
-            // Step 3: Check if we need to subtract or add the value
-            if (currValue < prevValue) {
-                total -= currValue; // Subtract
+    
+        int sum = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            int current = getValue(s.charAt(i));
+            
+            // Check if the next symbol represents a larger number
+            if (i + 1 < n && current < getValue(s.charAt(i + 1))) {
+                sum -= current;
             } else {
-                total += currValue; // Add
+                sum += current;
             }
-
-            prevValue = currValue; // Update previous value
         }
-
-        return total;
-
+        return sum;
     }
+
+   
 }
