82554a94d1102420d1030bd48f8eee275893a5f6
Atul Ahire <atulsureshahire@gmail.com>
2025-02-08 01:16:14 -0800

    Time: 31 ms (58.56%) | Memory: 51.3 MB (83.64%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -21,8 +21,8 @@ class Solution {
                     while(left < right && nums[left] == nums[left+1]) left++;
                     while(left < right && nums[right] == nums[right-1]) right--;
 
-                    left++;
-                    right--;
+                  left++;
+                  right--;
 
                 } else if (sum < 0)
                 {
