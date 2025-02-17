b699097869fb4622d14dbd2ab4d1d775983761de
Atul Ahire <atulsureshahire@gmail.com>
2025-02-17 02:17:07 -0800

    Time: 0 ms (100.00%) | Memory: 41.5 MB (76.08%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -2,18 +2,52 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length ==0) return "";
 
-        String prefix = strs[0]; // assign first string as prefix
+        // // horizontal scanning
+        // String prefix = strs[0]; // assign first string as prefix
         
-        for(int i=1 ; i < strs.length; i++)
-        {
-        while(strs[i].indexOf(prefix) !=0)
-        {
-            prefix = prefix.substring(0, prefix.length() -1);
+        // for(int i=1 ; i < strs.length; i++)
+        // {
+        // while(strs[i].indexOf(prefix) !=0)
+        //     {
+        //         prefix = prefix.substring(0, prefix.length() -1);
 
-            if(prefix.isEmpty())  return "";
-        }
+        //          if(prefix.isEmpty())  return "";
+        //     }
+        // }
+
+        // return prefix;
+
+        // we can use binary search for most efficient implementation
+        int left = 0;
+        int right = strs[0].length();
+
+        while(left < right)
+        {
+            int mid = (left + right + 1) / 2;
+
+            if(matchedPrefix(strs, mid))
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid -1;
+            } 
         }
 
-        return prefix;
+        return strs[0].substring(0, left);
+    }
+
+    private boolean matchedPrefix(String[] strs, int mid)
+    {
+        String prefix = strs[0].substring(0, mid);
+
+        for(int i=1; i < strs.length; i++)
+        {
+            if(!strs[i].startsWith(prefix)) 
+            return false;
+        }
+
+        return true;
     }
 }
